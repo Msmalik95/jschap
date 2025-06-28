91af2dfaa8118367973a6fb91f302b79e541c3c1
muhammad sami malik <muhammadsamimalik1995@gmail.com>
2025-06-29 00:31:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//Qno1
+//var a = ("username")
+//alert (a)
+
+//Qno2
+//var myName = ("MUHAMMAD SAMI MALIK")
+//alert (myName)
+
+//Qno3
+//var message = ("Hello World")
+//alert (message)
+
+//Qno4
+ //alert("muhammad sami malik")
+ //alert("30 years old")
+ //alert("certified mobile application development")
