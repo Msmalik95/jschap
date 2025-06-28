b1aa74622dc204e34f8a91c130030c1ed4fa436b
muhammad sami malik <muhammadsamimalik1995@gmail.com>
2025-06-29 00:29:41 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,18 @@
+//Qno1
+//var age = 30; 
+  //alert("my age is: " + age);
+
+//Qno2
+//var time = 10; 
+  //alert("you have visited this site: " + time);
+
+  //Qno3
+  //var birthYear = 1995
+  //document.write ("my birth year is " + birthYear)
+
+  //Qno4
+  //var visitorname = Msmalik
+  //var product = t-Shirt
+  //var quantity = 5
+
+  //Document.write (visitorname + " ordered " + quantity + " " + product + " on XYZ Clothing store.");
